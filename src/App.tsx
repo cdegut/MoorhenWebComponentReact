443fa3b8991f973627344d3e2475696ae9445f6d
Clement Degut <c.degut@gmail.com>
2026-05-12 10:19:00 +0100

    complete change to new webcomponent logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
 import { useEffect, useState } from "react";
+import { MoorhenWebComponentAttributes, registerMoorhenWebComponent } from "moorhen/web-component";
 import React from "react";
-import "./MyMoorhen";
 import "./App.css";
-import { MyMoorhen } from "./MyMoorhen";
+import { useMoorhenSelector, useWebComponentInstanceRef } from "moorhen/hooks";
+
+registerMoorhenWebComponent();
+
+declare module "react" {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace JSX {
+        interface IntrinsicElements {
+            "moorhen-web-component": MoorhenWebComponentAttributes;
+        }
+    }
+}
 
 function App() {
     const [width, setWidth] = useState(1024);
     const [height, setHeight] = useState(768);
+    const [backgroundColor, setBackgroundColor] = useState<[number, number, number]>([1, 1, 1]);
+
+    const moorhenInstanceRef = useWebComponentInstanceRef("my-moorhen");
+    const openedMolecules = useMoorhenSelector("my-moorhen", (state) => state.molecules.moleculeList);
+
     useEffect(() => {
-        const myMoorhenElement = document.getElementById("my-moorhen") as MyMoorhen;
-        const moorhenInstance = myMoorhenElement.moorhenInstance;
-        if (!moorhenInstance) {
+        if (!moorhenInstanceRef.current) {
             return;
         }
-        moorhenInstance.width = width;
-        moorhenInstance.height = height;
+        moorhenInstanceRef.current.width = width;
+        moorhenInstanceRef.current.height = height;
     }, [width, height]);
+
+    useEffect(() => {
+        if (!moorhenInstanceRef.current) {
+            return;
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!moorhenInstanceRef.current) {
+            return;
+        }
+        moorhenInstanceRef.current.sceneSettings.setBackgroundColor([...backgroundColor, 255]);
+    }, [backgroundColor]);
 
     return (
         <div className="app">
@@ -28,6 +55,9 @@ function App() {
                     This uses React version: <strong>{React.version}</strong> while Moorhen use version{" "}
                     <strong> 19.2.0</strong>
                 </p>
+                <p>
+                    Opened molecules: <strong>{openedMolecules?.length}</strong>
+                </p>
             </header>
 
             <div className="controls">
@@ -35,7 +65,7 @@ function App() {
                     Width
                     <input
                         type="number"
-                        min={200}
+                        min={0}
                         max={3000}
                         step={10}
                         value={width}
@@ -47,7 +77,7 @@ function App() {
                     Height
                     <input
                         type="number"
-                        min={200}
+                        min={0}
                         max={2000}
                         step={10}
                         value={height}
@@ -55,10 +85,43 @@ function App() {
                     />
                     px
                 </label>
+                <label>
+                    Background Colour RGB
+                    <input
+                        type="number"
+                        min={0}
+                        max={255}
+                        step={1}
+                        value={backgroundColor[0] * 255}
+                        onChange={(e) =>
+                            setBackgroundColor([Number(e.target.value) / 255, backgroundColor[1], backgroundColor[2]])
+                        }
+                    />
+                    <input
+                        type="number"
+                        min={0}
+                        max={255}
+                        step={1}
+                        value={backgroundColor[1] * 255}
+                        onChange={(e) =>
+                            setBackgroundColor([backgroundColor[0], Number(e.target.value) / 255, backgroundColor[2]])
+                        }
+                    />
+                    <input
+                        type="number"
+                        min={0}
+                        max={255}
+                        step={1}
+                        value={backgroundColor[2] * 255}
+                        onChange={(e) =>
+                            setBackgroundColor([backgroundColor[0], backgroundColor[1], Number(e.target.value) / 255])
+                        }
+                    />
+                </label>
             </div>
 
             <div className="viewer-container">
-                <my-moorhen id="my-moorhen" />
+                <moorhen-web-component width={width} height={height} id="my-moorhen" />
             </div>
         </div>
     );
